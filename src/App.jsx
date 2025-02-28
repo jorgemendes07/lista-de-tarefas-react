a4ce2e25a49e24ce18111287e9ff84507b889804
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-02-27 21:45:56 -0300

    adicionado estilo ao App

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,28 @@ const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div id='appContainer'>
       <h1>Lista de tarefas</h1>
-      <label htmlFor="tarefa"></label>
-      <input type="text" placeholder='Qual tarefa deseja incluir?' />
-      <button>Adicionar</button>
+      <div id='tarefaBotao'>
+        <input type="text" id='tarefa' placeholder='Qual tarefa deseja incluir?' />
+        <button id='botao'>Adicionar</button>
+      </div>
       <hr />
-      <section id='tarefas'></section>
+      <section id='tarefas'>
+        <div>
+          <span>O</span>
+          <span>Tarefa 1</span>
+          <span>*</span>
+          <span>X</span>
+        </div>
+        <div>
+          <span>O</span>
+          <span style={{ textDecoration: 'line-through' }} >Tarefa 2</span>
+          <span>*</span>
+          <span>X</span>
+        </div>
+        
+      </section>
     </div>
   )
 }
