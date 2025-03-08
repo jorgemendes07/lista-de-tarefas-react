693ed5e30b41051587a6c26dcd4ce2d38d4733d9
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-03-08 16:10:59 -0300

    adicionado controle de estado ao input de checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -69,6 +69,7 @@ const App = () => {
           <div key={tarefa.id}>
             <input 
             type="checkbox"
+            checked={tarefa.concluida}
             onChange={() => handleCheckbox(tarefa.id)}
             />
             <p 
